1b20fe64ee231f2fba01553bfbdff39befade6ab
qwertyriks <romeokumatse@gmail.com>
2023-08-21 12:48:31 -0700

    printf update_0

--- functions1.c
+++ functions1.c
@@ -1,1 +1,83 @@
 #include "main.h"
+
+/***** PRINT UNSIGNED NUMBER *****/
+/**
+ * print_unsigned - Print unsigned number
+ * @types: lits arguments
+ * @buffer: buffer array handles print
+ * @flag: calculates active flags
+ * @width: get width
+ * @precision: specifies the precision
+ * @size: size specifier
+ * Return: Number of chars printed
+ */
+int print_unsigned(va_list types, char buffer[],
+	int flag, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
+
+/***PRINT UNSIGNED NUMBER IN HEXADECIMAL***/
+
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789abcdef", buffer,
+		flags, 'x', width, precision, size));
+}
+/***PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL***/
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789ABCDEF", buffer,
+		flags, 'X', width, precision, size));
+}
+/***PRINT HEXX NUM IN LOWER OR UPPEer***/
+int print_hexa(va_list types, char map_to[], char buffer[],
+	int flags, char flag_ch, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+	{
+		buffer[i--] = flag_ch;
+		buffer[i--] = '0';
+	}
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
