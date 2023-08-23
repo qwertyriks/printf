5c8830c8b7ef349951f961efb1ec15e8e6efb801
“Souka45” <soumiakazdar456@gmail.com>
2023-08-23 09:47:13 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -7,25 +7,26 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars.
  */
-int _printf (const char *format, ...)
+int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
-	if(format == NULL)
+	if (format == NULL)
 		return (-1);
 
 	va_start(list, format);
 
-	for (i=0; format && format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -50,6 +51,11 @@ int _printf (const char *format, ...)
 
 	return (printed_chars);
 }
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
